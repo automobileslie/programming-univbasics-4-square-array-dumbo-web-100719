f568f6d79be7297e612bbfe9a88628e245461cd8
Carlie Anglemire <carlie.anglemire@gmail.com>
2019-09-17 18:29:02 +0000

    Automatically backed up by Learn

--- lib/square_array.rb
+++ lib/square_array.rb
@@ -4,9 +4,9 @@ def square_array(array)
   empty_array = []
   counter = 0
   while array[counter] do 
-  empty_array.push((array[counter])**(array[counter]))
+  return empty_array.push((array[counter])**(array[counter]))
     counter+=1
   end
-    return empty_array
+    
 end
 
